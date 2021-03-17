5242a38805f211f79b09ab5e4ba0c186ba22f0e4
eridian <eridiandark@gmail.com>
2021-03-17 11:15:15 +0200

    2 commit

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Calculator from './components/calculator/scripts/calculator';
-
-ReactDOM.render(
-    <Calculator onSave={ value => alert('Saved ' + value) }/>,
-    document.getElementById("root")
-);
